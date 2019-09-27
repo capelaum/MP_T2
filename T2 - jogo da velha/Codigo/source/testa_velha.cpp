e65da63c7fd5da1f45234e5cf73e36fe8b5d9ddc
capelaum <thecapellett@gmail.com>
2019-09-27 16:52:00 -0300

    Criado teste de vencedor X na diagonal 1

--- T2 - jogo da velha/Codigo/source/testa_velha.cpp
+++ T2 - jogo da velha/Codigo/source/testa_velha.cpp
@@ -163,6 +163,31 @@ TEST(Jogo_da_Velha, Vencedor_X_coluna_2) {
     ASSERT_NE(1, Verifica_Velha(Velha, 3));
 }
 
+// testa se vencedor foi X na diagonal 1
+TEST(Jogo_da_Velha, Vencedor_X_diagonal_1) {
+    int Velha[3][3];
+    int i = 0, j = 0;
+
+    // zera matriz
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            Velha[i][j] = 0;
+        }
+    }
+
+    // completa diagonal com valor 1
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            if (i == j) {
+                Velha[i][j] = 1;
+            }
+        }
+    }
+
+    // testa função com diagonal 1 preenchida com 1
+    ASSERT_EQ(1, Verifica_Velha(Velha, 3));
+}
+
 int main(int argc, char** argv) {
     // Inicializa os testes
     ::testing::InitGoogleTest(&argc, argv);
