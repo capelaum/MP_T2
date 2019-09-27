d56c2714197001fc52b127ff6d76d0d4a5bbe037
capelaum <thecapellett@gmail.com>
2019-09-27 17:11:33 -0300

    Caso de teste em que diagonal 2 nao esta apenas com valores 1

--- T2 - jogo da velha/Codigo/source/testa_velha.cpp
+++ T2 - jogo da velha/Codigo/source/testa_velha.cpp
@@ -188,7 +188,7 @@ TEST(Jogo_da_Velha, Vencedor_X_diagonal_1) {
     ASSERT_EQ(1, Verifica_Velha(Velha, 3));
 
     // muda diagonal 1
-    Velha[2][2] = 2;
+    Velha[1][1] = 2;
 
     // caso em que X nao deve vencer
     ASSERT_NE(1, Verifica_Velha(Velha, 3));
@@ -220,6 +220,12 @@ TEST(Jogo_da_Velha, Vencedor_X_diagonal_2) {
 
     // testa função com diagonal 1 preenchida com 1
     ASSERT_EQ(1, Verifica_Velha(Velha, 3));
+
+    // muda diagonal 2
+    Velha[0][2] = 2;
+
+    // caso em que X nao deve vencer
+    ASSERT_NE(1, Verifica_Velha(Velha, 3));
 }
 
 int main(int argc, char** argv) {
