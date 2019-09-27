5bea3c396de304aa82c3b7d7377a23632a7a4f7a
Capelaum <thecapellett@gmail.com>
2019-09-27 02:33:25 -0300

    Inicio
    TDD de uma função que verifica o resultado de um jogo da velha representada por uma matriz 3x3

---
+++ T2 - jogo da velha/Codigo/include/velha.h
@@ -0,0 +1,12 @@
+#ifndef VELHA_H
+#define VELHA_H
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+
+
+
+#endif
---
+++ T2 - jogo da velha/Codigo/source/testa_velha.cpp
@@ -0,0 +1,11 @@
+
+#include "velha.h"
+
+int main(int argc, char** argv)
+{
+    // Inicializa os testes
+	::testing::InitGoogleTest (&argc, argv);
+	return RUN_ALL_TESTS();
+
+    return 0;
+}
