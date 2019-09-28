346e5057ff3399882e174fadd4c6b4db05d9fef0
capelaum <thecapellett@gmail.com>
2019-09-27 21:41:00 -0300

    Criado teste de vencedor O na linha 1

--- T2 - jogo da velha/Codigo/source/testa_velha.cpp
+++ T2 - jogo da velha/Codigo/source/testa_velha.cpp
@@ -258,6 +258,33 @@ TEST(Jogo_da_Velha, Vencedor_O_linha_0) {
     ASSERT_NE(2, Verifica_Velha(Velha, 3));
 }
 
+// testa se vencedor foi O na linha 1
+TEST(Jogo_da_Velha, Vencedor_O_linha_1) {
+    int Velha[3][3];
+    int i = 0, j = 0;
+
+    // zera matriz
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            Velha[i][j] = 0;
+        }
+    }
+
+    // completa linha 0 com 2
+    for (j = 0; j < 3; j++) {
+        Velha[1][j] = 2;
+    }
+
+    // testa função com linha 0 preenchida com 2
+    ASSERT_EQ(2, Verifica_Velha(Velha, 3));
+
+    // muda linha 1
+    Velha[1][0] = 1;
+
+    // caso em que O nao deve vencer
+    ASSERT_NE(2, Verifica_Velha(Velha, 3));
+}
+
 int main(int argc, char** argv) {
     // Inicializa os testes
     ::testing::InitGoogleTest(&argc, argv);
